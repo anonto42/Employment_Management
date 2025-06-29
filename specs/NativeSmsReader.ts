a07fe5599e490ad3c8535d37e880eae7933bd947
Sohidul Islam Ananto <anontom90@gmail.com>
2025-06-29 22:21:48 +0600

    code updating

--- specs/NativeSmsReader.ts
+++
@@ -1,8 +0,0 @@
-import type { TurboModule } from 'react-native';
-import { TurboModuleRegistry } from 'react-native';
-
-export interface Spec extends TurboModule {
-  readSms(): Promise<Array<{ address: string; body: string; timestamp: number }>>;
-}
-
-export default TurboModuleRegistry.get<Spec>('NativeSmsReader') as Spec;
